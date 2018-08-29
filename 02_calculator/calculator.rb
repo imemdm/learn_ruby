07a440e157d53e419e09226cf4f65dbc7b74bf2c
Emil Dimitrov <edimitrov1@gmail.com>
2018-08-29 15:02:12 +0300

    Calculator done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,41 @@
-#write your code here
+def add a, b
+  a + b
+end
+
+def subtract a, b
+  a - b
+end
+
+def sum params
+  all = 0
+  if params.empty? 
+    all
+  end
+
+  params.each do |p|
+    all += p
+  end
+
+  all
+end
+
+def multiply params
+  product = 1
+  params.each do |p|
+    product *= p
+  end
+
+  product
+end
+
+def power n, pow
+  n**pow
+end
+
+def factorial n
+  if n == 1 || n == 0
+    1
+  else
+    n = factorial(n - 1) * n
+  end
+end
