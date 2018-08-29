982c5e28f790f3234440da5521eb183254110976
Emil Dimitrov <edimitrov1@gmail.com>
2018-08-29 18:13:57 +0300

    Simon done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
-#write your code here
+def echo word
+  word
+end
+
+def shout word
+  word.upcase
+end
+
+def repeat word, n = 2
+  str = "#{word} " * n
+  str.strip
+end
+
+def start_of_word word, n
+  word[0...n]
+end
+
+def first_word input
+  words = input.split
+  words[0]
+end
+
+def titleize input
+  words = input.split
+  caps = words.map.with_index do |word, i|
+    unless i != 0 && (word == "and" || word == "the" || word == "over")
+      word.capitalize
+    else
+      word
+    end
+  end
+  caps.join(" ")
+end
