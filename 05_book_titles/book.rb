ee7614a1370c9c1d3a3153942f15cf162af6d292
Emil Dimitrov <edimitrov1@gmail.com>
2018-08-30 19:17:05 +0300

    Book titles done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  attr_accessor :title
+  @@no_caps = ["the", "an", "a", "and", "in", "of"]
+
+  def title=(value)
+    @title = capitalize(value)
+  end
+
+  def capitalize input
+    words = input.split
+    caps = words.map.with_index do |word, i|
+      unless i != 0 && @@no_caps.include?(word)
+        word.capitalize
+      else
+        word
+      end
+    end
+    caps.join(" ")
+  end
 end
