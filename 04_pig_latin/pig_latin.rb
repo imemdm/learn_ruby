8678bb9f1a510bbfa1093e61025dc0d7b5eb7472
Emil Dimitrov <edimitrov1@gmail.com>
2018-08-30 14:44:47 +0300

    Pig Latin done

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,34 @@
-#write your code here
+def translate str
+  words = str.split
+
+  converted = words.map do |word|
+    convert(word)
+  end
+
+  converted.join(" ")
+
+end
+# quiet
+
+def convert word
+  chars = word.split("")
+  vowel_index = chars.index { |el| vowel? el }
+  result = []
+  
+  if chars[vowel_index - 1] == "q" && chars[vowel_index] == "u"
+    vowel_index = chars[0..vowel_index].length + chars[vowel_index + 1...chars.length].index { |el| vowel? el }
+  end
+
+  vowels = chars[vowel_index..chars.length]
+  cons = chars[0...vowel_index]
+  ay = ["ay"]
+
+  result.concat(vowels, cons, ay)
+
+  result.join("")
+end
+
+def vowel? c
+  vowels = ["a", "e", "i", "o", "u"]
+  vowels.include?(c)
+end
