5fef9a53ea76b9d7e93f550c7ba7d30fc68ba3a5
Emil Dimitrov <edimitrov1@gmail.com>
2018-08-31 18:14:07 +0300

    Timer done

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds=(value)
+    @seconds = value
+  end
+
+  def time_string
+    seconds = @seconds % 60
+    minutes = (@seconds / 60) % 60
+    hours = ((@seconds / 60) / 60) % 60
+
+    "#{padded hours}:#{padded minutes}:#{padded seconds}"
+  end
+  
+  def padded num
+    num.to_s.rjust(2, "0")
+  end
 end
